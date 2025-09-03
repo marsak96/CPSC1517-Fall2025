f5fb59d678853762a5a937dfcdc89249c9f443c4
Marouen Sakouhi <marouensakouhi1@gmail.com>
2025-09-02 22:17:45 -0600

    comments added

--- OOPsSolution/OOPsReview/Employment.cs
+++ OOPsSolution/OOPsReview/Employment.cs
@@ -8,5 +8,36 @@ namespace OOPsReview
 {
     internal class Employment
     {
+        //data members
+        //aka fields, variables, attributes
+        //typically data members are private and hold data for use
+        //  within your class
+        //usually associated with a property
+        //a data member does not have any built-in validation
+        private string _Title;
+        private int _Year;
+
+        //properties
+        //are associated with a single piece of data.
+        //Properties can be implemented by:
+        //  a) fully implemented property
+        //  b) auto implemented property
+
+        //A property does not need to store data
+        //  this type of property is referred to as a read-only
+        //  this property typically uses existing data values
+        //      within the instance to return a computed value 
+        // NOTE there would be NO set for the property
+
+        //Assume two data fields _FirstName and _LastName
+        //public string FullName
+        //{
+        //    get { return _FastName + " " + _LastName; }
+        //}
+
+        //constructors
+
+
+        //methods
     }
 }
